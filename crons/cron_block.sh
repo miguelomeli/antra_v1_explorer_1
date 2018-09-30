f942de48ea7113ef142467074f02ae0266a862f0
Miguel Lomeli i9 <miguel@lomeli.io>
2018-09-30 11:41:31 -0500

    crons

---
+++ crons/cron_block.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/usr/bin/node /var/www/explorer/scripts/sync.js index update
+rm /var/www/explorer/tmp/index.pid
+sleep 20
+/usr/bin/node /var/www/explorer/scripts/sync.js index update
+rm /var/www/explorer/tmp/index.pid
